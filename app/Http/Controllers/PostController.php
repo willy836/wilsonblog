44493ba4dbbe3ca0969d58a0daf68c30364dd958
willy836 <wilsonkimiri836@gmail.com>
2023-07-28 17:00:46 +0300

    PostController

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $posts = Post::orderBy('created_at', 'desc')->filter(request(['search']))->get();
+        $categories = Category::all();
+
+        return view('posts.index', [
+            'posts' => $posts,
+            'categories' => $categories
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'title' => 'required | min:3 |max:20',
+            'body' => 'required |min:50',
+            'category_id' => 'required'
+        ]);
+
+        Post::create($validatedData);
+
+        return redirect()->route('posts.index')->with('success', 'Post was created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $post = Post::find($id);
+
+        if(!$post){
+            abort(404);
+        }
+
+        return view('posts.show', [
+            'post' => $post
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $post = Post::find($id);
+
+        if(!$post){
+            abort(404);
+        }
+
+        return view('posts.edit', [
+            'post' => $post
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $post = Post::find($id);
+
+        $validatedData = $request->validate([
+            'title' => 'required | min:3 |max:20',
+            'body' => 'required |min:50'
+        ]);
+        
+        $post->update($validatedData);
+
+        return redirect()->route('posts.index')->with('success', 'Post was updated successfully.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $post = Post::find($id);
+
+        $post->delete();
+
+        return redirect()->route('posts.index')->with('success', 'Post was deleted successfully.');
+    }
+}
