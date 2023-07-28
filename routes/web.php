dc2639304977d77c37d1a2c59d86d022783486df
willy836 <wilsonkimiri836@gmail.com>
2023-07-28 17:09:35 +0300

    Routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('posts', PostController::class);
+
+Route::get('category/{category_id}/posts', [CategoryController::class, 'showPosts'])->name('category.posts');
+
+Route::get('authors/{author}', [UserController::class, 'showAuthorPosts'])->name('authors.user_id');
