f1decdcd75ffa92917647436db5551ac6167de0b
willy836 <wilsonkimiri836@gmail.com>
2023-07-29 11:44:57 +0300

    Create comment model and migration

--- database/migrations/2023_07_29_082524_create_comments_table.php
+++ database/migrations/2023_07_29_082524_create_comments_table.php
@@ -11,13 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('post_id')->constrained()->cascadeOnDelete();
             $table->foreignId('user_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
-            $table->string('title');
             $table->text('body');
-            $table->string('image');
             $table->timestamps();
         });
     }
@@ -27,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('comments');
     }
 };
